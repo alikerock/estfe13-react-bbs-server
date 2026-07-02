a3f050f3be3da4aa802ccedb0399a99dd8719bb6
alikerock <alikerock@gmail.com>
2026-07-02 14:50:31 +0900

    Set up MySQL connection and add endpoint to fetch board list

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const mysql = require("mysql");
 const port = 3000;
 
 let corsOptions = {
@@ -9,9 +10,27 @@ let corsOptions = {
 
 app.use(cors(corsOptions));
 
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "12345",
+  database: "bbs",
+});
+
+db.connect();
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+app.get("/list", (req, res) => {
+  const sqlQuery = "SELECT * FROM board;";
+  db.query(sqlQuery, (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  });
+});
+
+db.end();
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
